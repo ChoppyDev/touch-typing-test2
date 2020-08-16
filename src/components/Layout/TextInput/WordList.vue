330d3d8754ebfee1ecfd368ab7fdd7b72c34ce1f
Nicolas KLEIN <nicolas6720@gmail.com>
2020-08-16 23:10:13 +0200

    ✨ created the point system, the array generator, and a lot of code

--- src/components/Layout/TextInput/WordList.vue
+++ src/components/Layout/TextInput/WordList.vue
@@ -1,20 +1,40 @@
 <template>
   <div class="text-input-wrapper">
-    <input type="text" name="text input" id="" v-on:keyup.space="onSpace">
+    <ul id="example-1">
+  <li v-for="word in wordArray" :key="word.index">
+    {{ word }}
+  </li>
+</ul>
+    <input  v-model="word" type="text" name="text input" id="" v-on:keyup.space="onSpace">
+    <p >{{points}} fdsfss</p>
   </div>
 </template>
 
 <script>
-import {generateList} from "../../../scripts/Words"
+//import {generateList} from "../../../scripts/Words"
+import {WordManager} from "../../../scripts/Words"
 export default {
   name : 'WordList',
   created() {
-    console.log(generateList())
+    this.wordManager = new WordManager(20);
+    this.wordArray = this.wordManager.wordlist;
+    this.points = this.wordManager.points;
+    //console.log(wordManager)
+    console.log(this.wordManager.wordlist)
+  },
+  data() {
+    return {
+      word: null,
+      wordManager: null,
+      wordArray: null,
+      points: null
+    };
   },
   methods : {
     onSpace(){
-      console.log("space")
-      console.log(this.$el.innerHTML)
+      this.wordManager.compareWord(this.word)
+      console.log(this.wordManager.wordlist)
+      this.word = ""
     } 
   }
 }
--- src/scripts/Words.js
+++ src/scripts/Words.js
@@ -9,3 +9,41 @@ function generateList() {
 }
 
 export {generateList}
+
+
+class WordManager {
+  wordArray = [];
+  points = 0; 
+  wordCount = 0;
+
+  constructor(nb) {
+    for (var i = 0; i < nb; i++) {
+      this.wordCount = nb;
+      this.wordArray.push(list.Fr[Math.floor(Math.random() * list.Fr.length)]);
+    }
+  }
+
+  get wordlist() {
+    return this.wordArray;
+  }
+  get points() {
+    return this.points;
+  }
+
+  // created the method that remov
+  compareWord(word) {
+    var ensure = word.replace(/\s/g, '');
+
+      if(this.wordArray[0] == ensure) {
+        this.wordArray.splice(0, 1);
+        this.points += 1;
+        //console.log(this.wordArray)
+        console.log("true");
+        return true
+      }
+      console.log("false");
+      return false
+  }
+}
+
+export {WordManager}
